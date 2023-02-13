0ad506d473172209e09d031cf0deee96cf483304
XRP Fact Checker <xrpfactchecker@gmail.com>
2023-02-13 14:04:02 -0500

    Use DB_SCHEMA environment variable when connecting to the database.

--- http.js
+++ http.js
@@ -215,7 +215,7 @@ function getDb() {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "greyhounds"
+    database: process.env.DB_SCHEMA
   });
 }
 
