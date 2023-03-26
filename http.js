5dae969f1c934e421d88cdc7cc5ee4eee6a29ede
XRP Fact Checker <xrpfactchecker@gmail.com>
2023-03-25 20:37:11 -0400

    Revert "Change how it is getting the address from the seed when minting to be the same as the offer"
    This reverts commit 64175dfd77a99c2fbaac5dcca4f5185a622627cf.

--- http.js
+++ http.js
@@ -1037,10 +1037,8 @@ async function mintNft(cid) {
     try {
       console.log("Minting NFT: " + cid);
       const client = new XrplClient(process.env.XRPL_RPC);
-
-      const secret = process.env.WALLET_SECRET;
-      const wallet = xrpl.Wallet.fromSeed(secret);
-      const address = wallet.classicAddress;
+      const wallet = derive.familySeed(process.env.WALLET_SECRET);
+      const address = wallet.address;
       const req = await client.send({ command: 'account_info', account: address })
       const sequence = req.account_data.Sequence;
 
