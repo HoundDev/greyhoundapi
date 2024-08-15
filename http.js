cb5a3d6212035a745dc98aebafde99b84d9545da
Aaditya-T <aadityathakkar47@gmail.com>
2024-08-15 12:01:31 +0530

    db

--- http.js
+++ http.js
@@ -407,7 +407,8 @@ function getDbStaked() {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_SCHEMA_STAKED
+    // database: process.env.DB_SCHEMA_STAKED
+    database: "greyhoun_testing"
   });
 }
 
